9a5e264d514adf4f829142e859d0987a1e96ca3c
Anh Phan Duc (SM) <aphanduc@deloitte.cs>
2023-01-18 22:14:30 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Phan Duc Anh
+              Phan Duc Anh test build
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
